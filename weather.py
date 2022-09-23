85def11ae7a4acd2ecafbbecf7c5f031d3fb4b19
DucaData <dubravkatabakovic@gmail.com>
2022-09-23 17:48:36 +0200

    created gitignore

--- weather.py
+++ weather.py
@@ -1,2 +1,11 @@
 import json
+from credentials import appid
 #creating weather app
+import sys
+
+if len(sys.argv)<2:
+    grad=input("Choose town: ")
+else:
+    grad=sys.argv[1]
+
+print(sys.executable)
